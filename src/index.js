38f1ce61a0a4040549f5167e0729bd18b31ff0ef
lvsally <sally1215115346>
2022-11-07 16:36:43 -0500

    feat: 高阶组件复用属性

--- src/index.js
+++ src/index.js
@@ -1,60 +1,29 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let ColorTheme = React.createContext()
-
-class Header extends React.Component {
-  render() {
-    return <ColorTheme.Consumer>
-      {({color, setColorFn}) => <div style={{height: 100, with: 300, border: `4px solid ${color}` }}>
-        header
-        <button onClick={() => setColorFn('black')}>变黑</button>
-        <button onClick={() => setColorFn('red')}>变红</button>
-      </div>}
-      
-    </ColorTheme.Consumer>
-  }
-}
-
-class Main extends React.Component {
-  static contextType = ColorTheme
-  render() {
-    return <div style={{height: 300, with: 300, border: `4px solid ${this.context.color}` }}>
-      main
-      <button onClick={() => this.context.setColorFn('black')}>变黑</button>
-      <button onClick={() => this.context.setColorFn('red')}>变红</button>
+class Children extends React.Component{
+  render () {
+    return <div>
+      <div onClick={this.props.show}>show</div>
+      <div onClick={this.props.hide}>hide</div>
     </div>
   }
 }
 
-class Parent extends React.Component {
-  state = {
-    color: 'red'
-  }
-  setColorFn = (color) => {
-    console.log(color)
-    this.setState({
-      color
-    })
-  }
-  render() {
-    let {color} = this.state
-    let context = {setColorFn: this.setColorFn, color}
-    console.log(color, 'inner')
-
-    return <ColorTheme.Provider value={context}>
-      <div style={{height: 500, with: 500, border: `4px solid ${color}` }}>
-        {color}
-        <Header></Header>
-        <Main></Main>
-      </div>
-    </ColorTheme.Provider>
+function HComponent(Component) {
+  return class extends React.Component {
+    state={
+      show: () => console.log('show'),
+      hide: () => console.log('hide')
+    }
+    render() {
+      return <Component {...this.state} />
+    }
   }
 }
-const element = <Parent />
-// const element = <div a='bbb'>133</div>
 
+const Element = HComponent(Children)
 ReactDOM.render(
-  element,
+  <Element/>,
   document.getElementById('root')
 );
