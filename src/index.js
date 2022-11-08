96d2e71df4b1995d918bebb79183255b2402f5b0
lvsally <sally1215115346>
2022-11-08 16:48:36 -0500

    feat: use portal

--- src/index.js
+++ src/index.js
@@ -2,29 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Mouse extends React.Component{
-  state = {
-    x: -1,
-    y: -1
-  }
-  moveFn = (event) => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY,
-    })
-  }
-
+  
   render () {
-    return <div onMouseMove={this.moveFn} style={{border: '1px solid #000'}}>move: {this.props.children(this.state)}</div>
+    return <div style={{border: '1px solid #000'}}>
+      child-out
+      {console.log(document.querySelector('#child'))}
+      {document.querySelector('#child') && ReactDOM.createPortal(<div>child from portal</div>, document.querySelector('#child'))}
+    </div>
   }
 }
 
 class Parent extends React.Component{
-  
+  state = {
+    num: -1
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({num: 1})
+    })
+  }
   render () {
     return <div>
-      <Mouse>
-        {({x, y}) => <div>xxoo, {x}, {y}</div>}
-      </Mouse>
+      <div>parent</div>
+      <div id="child"></div>
+      {this.state.num===1 && <Mouse/>}
     </div>
   }
 }
