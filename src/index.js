db52bff8178f93793557d1369d3fb8f0564c5282
lvsally <sally1215115346>
2022-11-10 20:15:39 -0500

    feat:useMemo & useCallback

--- src/index.js
+++ src/index.js
@@ -1,26 +1,64 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function UseReducer() {
-  let reducer = (state, action) => {
-    switch(action) {
-      case 'add':
-        return {num: state.num + 1}
-      case 'minus':
-        return {num: state.num - 1}
-      default:
-        return state.num
-    }
-  }
-  const [state, dispatch] = React.useReducer(reducer, {num: 0})
+// ------- not use memo --------
+// function Child() {
+//   console.log(111)
+//   return <div>Child</div>
+// }
+
+// function Parent() {
+//   const [num, setNum] = React.useState(0)
+//   const [txt, setTxt] = React.useState('xx')
+
+//   let handlerFn = () => {
+//     setNum(num+1)
+//   }
+//   return <div>
+//     <div>parent: <input value={txt} onChange={(e) => {
+//       setTxt(e.target.value)
+//     }} /></div>
+//     <Child data={num} handler={handlerFn} />
+//   </div>
+// }
+
+// ----------------------------
+// 1 使用React.memo阻止没有state依赖的组件更新
+// 2 但是如果有state依赖，进一步阻止更新需要使用 React.useCallback, React.useMemo
+
+
+function Child(props) {
+  console.log('child update')
   return <div>
-    <div>A:{state.num}</div>
-    <div onClick={() => dispatch('add')}>+</div>
-    <div onClick={() => dispatch('minus')}>-</div>
+    Child: {props.data} 
+    <div onClick={props.handler}>+</div>
+  </div>
+}
+
+const ChildUseMemo = React.memo(Child)
+
+function Parent() {
+  const [num, setNum] = React.useState(0)
+  const [txt, setTxt] = React.useState('xx')
+
+  const handlerFn = React.useCallback(() => {
+    setNum(num+1)
+  }, [num])
+
+  // const handlerFn = () => {
+  //   setNum(num+1)
+  // }
+
+  const dataUseMemo = React.useMemo(() => num, [num])
+  return <div>
+    <div>parent: <input value={txt} onChange={(e) => {
+      setTxt(e.target.value)
+    }} /></div>
+    <ChildUseMemo data={dataUseMemo} handler={handlerFn} />
   </div>
 }
 
 ReactDOM.render(
-  <UseReducer/>,
+  <Parent/>,
   document.getElementById('root')
 );
